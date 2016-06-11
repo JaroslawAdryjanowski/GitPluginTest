c9dae2ccb50794adac779906db8f37303b700980
Jaroslaw Adryjanowski <adryjanowski@gmail.com>
2016-06-11 16:26:17 +0100

    Zmiana 2 bug#1234

--- src/packt/jee/eclipse/git/GitTestApp.java
+++ src/packt/jee/eclipse/git/GitTestApp.java
@@ -5,7 +5,8 @@ public class GitTestApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git!");
-		System.out.println("Zmiana 1");
+		System.out.println("Zmiana 2");
+		System.out.println("Hello Git, from branch bug#1234 !!");
 
 	}
 
